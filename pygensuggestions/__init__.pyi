253c624111f896d0a49d85d0cab8272fd5f63712
Jonathan Dung <jonathandung@yahoo.com>
2026-06-20 15:33:25 +0800

    add uv.lock

--- pygensuggestions/__init__.pyi
+++ pygensuggestions/__init__.pyi
@@ -1,7 +1,7 @@
-__all__ = 'suggest',
 from . import lib # noqa: F401
 from collections.abc import Iterable, Sized
 from ty_extensions import Intersection
 from typing import AnyStr, Final
+__all__ = 'suggest',
 def suggest(candidates: Intersection[Sized, Iterable[AnyStr]], item: AnyStr, /, *, skip_identical: bool = True, respect_bounds: bool = True) -> AnyStr|None: ...
 __version__: Final[str]
